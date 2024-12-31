2ed515a6bef7091b6e969b0c174045d7f0a6ceda
kshekardataengineer <kshekardataengineer@gmail.com>
2024-12-31 08:39:10 +0000

    made changes as per demo version

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,8 +13,8 @@ import 'package:konktapp/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp(initialRoute: '',));
-
+    //await tester.pumpWidget(MyApp(initialRoute: '',));//for new code
+    await tester.pumpWidget(const MyApp(initialRoute: '',));
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
